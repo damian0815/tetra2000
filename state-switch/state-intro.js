b2f9a2c9fbec8b6f242d19a6e042d42ae04dfc69
Damian at mba <damian@frey.NOSPAMco.nz>
2014-08-23 17:06:47 +0200

    trying to make state-switching work

---
+++ state-switch/state-intro.js
@@ -0,0 +1,26 @@
+
+function IntroState() {
+
+	console.log('intro state ctor');
+
+	this.preload =	function() {
+
+		console.log('intro state preload');
+
+	}
+
+	this.create = function() {
+
+		console.log('intro state create');
+
+	}
+
+	this.update = function() {
+
+
+	}
+
+}
+
+
+
