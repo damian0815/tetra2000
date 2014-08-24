8b0fef3452738f1964cf4e26245d2ca5be91563f
Damian at mba <damian@frey.NOSPAMco.nz>
2014-08-24 21:58:43 +0200

    fix collisions on bus_station scene

--- scenes/bus_station/bus_station.js
+++ scenes/bus_station/bus_station.js
@@ -2,35 +2,24 @@
 function BusStation(game) {
 
 	this.preload = function(){
-		game.load.tilemap('map', 'assets/bus_station.json', null, Phaser.Tilemap.TILED_JSON);
+		game.load.tilemap('bus-map', 'assets/bus_station.json', null, Phaser.Tilemap.TILED_JSON);
 		game.load.image('bus_station', 'assets/busfun.png' );
 
 		// player
 		game.load.image('tetra', '../../shared/assets/ld30_front_standing.png');
 
-
-		// items
-		/*
-		game.load.image('pictureframe',
-		                'assets/koffer_packen_pictureframe.png');
-		game.load.image('flowerpot', 'assets/koffer_packen_flowerpot.png');
-		game.load.image('suitcase', 'assets/koffer_packen_suitcase.png');
-		game.load.image('teddy', 'assets/koffer_packen_teddy.png');
-		*/
-
-		game.load.image('cat', 'assets/koffer_packen_cat.png');
-
 	}
 
 	this.create = function() {
-		this.map = game.add.tilemap('map');
+		this.map = game.add.tilemap('bus-map');
 		// attach the tiles image to the map
 		this.map.addTilesetImage('bus_station', 'bus_station');
 
+		this.collisionLayer = this.map.createLayer('collision');
 		var bgLayer = this.map.createLayer('Tile Layer 1');
 		// resize the world bounds to match the layer
 		bgLayer.resizeWorld();
-		this.collisionLayer = this.map.createLayer('collision');
+		//this.collisionLayer.debug = true;
 
 		// load player sprite
 		this.player = game.add.sprite(16, bgLayer.height-16, 'tetra');
@@ -46,18 +35,29 @@ function BusStation(game) {
 
 		this.map.setCollision([ 106 ], true, this.collisionLayer);
 
-		// suitcase
-		/*
-
-		 */
+	}
 
 
+	this.update = function() {
 
+		// check for map collisions
+		game.physics.arcade.collide(this.player, this.collisionLayer);
 
-		// collectible objects
-		this.items = game.add.group();
-		this.items.enableBody = true;
-
+		// update movement
+		this.player.body.velocity.x = 0;
+		this.player.body.velocity.y = 0;
+		if ( this.cursors.left.isDown ) {
+			this.player.body.velocity.x = -150;
+		}
+		else if ( this.cursors.right.isDown ) {
+			this.player.body.velocity.x = 150;
+		}
+		else if ( this.cursors.up.isDown ) {
+			this.player.body.velocity.y = -150;
+		}
+		else if ( this.cursors.down.isDown ) {
+			this.player.body.velocity.y = 150;
+		}
 
 
 	}
